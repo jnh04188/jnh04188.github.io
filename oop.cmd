f13a33df170d33da0ae22c6b6085c8649e0444a0
jnh04188 <42957626+jnh04188@users.noreply.github.com>
2018-11-13 15:40:59 +0900

    Delete oop.cmd

--- oop.cmd
+++
@@ -1,5 +0,0 @@
-# 수업
-
-## ppt.08  object oriented programming
-
-![image](http://mail.naver.com/read/image/?mailSN=15865&attachIndex=2&contentType=image/jpeg&offset=1255&size=4790598&mimeSN=1541488740.91962.22778.50944&org=1&u=jnh04188.png)
